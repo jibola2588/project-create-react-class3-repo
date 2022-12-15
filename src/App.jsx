3a0530e4b8e731302ebe715a10d5a655007b52d8
Danelohim <Danielpekpo22@gmail.com>
2022-12-15 07:44:44 +0100

    Trying to pull latest v of repo3

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ import Parent from './pages/Parent';
 const App = () => {
   return (
     <div>
-      <Parent />
+      {/* <Parent /> */}
   
-      {/* <nav className='flex bg-indigo-400 text-white py-4 px-4 gap-4 cursor-pointer'>
+      <nav className='flex bg-indigo-400 text-white py-4 px-4 gap-4 cursor-pointer'>
           <Link to='/'>
                 Home
           </Link>
@@ -29,7 +29,7 @@ const App = () => {
          <Route path = '/' element= {<Home />} />
          <Route path = '/about' element= {<About />} />
          <Route path = '/contact' element= {<Contact />} />
-      </Routes> */}
+      </Routes>
     </div>
   );
 }
